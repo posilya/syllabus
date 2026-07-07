18afc6fe29e1496c317e39fa6564f9e500daa5f1
Ilya Posazhennikov <pos_ilya@vk.com>
2026-07-08 03:43:19 +0500

    Сделана тестовая страничка отображения расписания
    да, плохо, но зато теперь могу идти спать

--- routers/index.js
+++ routers/index.js
@@ -5,7 +5,9 @@ import { getGroupScheduleByInterval } from '../models/lesson_schedule.js';
 const router = new Router();
 
 router.get('/', async (req, res) => {
-    res.send(JSON.stringify(await getGroupScheduleByInterval(1, '2024-10-01', '2024-10-07')));
+    res.render('schedule/group', {
+        schedule: await getGroupScheduleByInterval(1, '2024-10-01', '2024-10-07')
+    });
 });
 
 export default router;
