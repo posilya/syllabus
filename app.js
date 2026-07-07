a3b0a4c4081cb509161a28c0888b8892e54a6285
Ilya Posazhennikov <pos_ilya@vk.com>
2026-07-08 03:42:03 +0500

    Подключение Handlebars

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import hbs from 'hbs';
 
-import router from './routers/index.js'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import router from './routers/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = (process.env.PORT) || 3000;
+const viewsPath = path.join(__dirname, 'views');
 
 app.use('/', router);
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(path.join(viewsPath, 'partials'));
 
 app.listen(port, () => {
     console.log(`http://127.0.1:${port}/`)
