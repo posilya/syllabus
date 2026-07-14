8dbcdf5d5b50ae0da9537b36d118f38ae76fd94a
Ilya Posazhennikov <pos_ilya@vk.com>
2026-07-14 17:49:22 +0000

    Добавлено форматирование даты и времени занятий

--- models/sql/group/get/schedule_by_interval.sql
+++ models/sql/group/get/schedule_by_interval.sql
@@ -2,14 +2,13 @@ SELECT
 	ls.id AS lesson_schedule_id,
 	t.id  AS teacher_id,     -- TODO Уточнить, норм ли брать id
 	a.id  AS auditorium_id,  --      из правых таблиц?
-	ls.lesson_date,
-	ls.time_start,
-	ls.time_end,
+	to_char(ls.lesson_date, 'DD.MM.YYYY') AS lesson_date,
+	to_char(ls.time_start, 'HH24:MI') AS time_start,
+	to_char(ls.time_end, 'HH24:MI') AS time_end,
 	a.auditorium_number,
 	a.floor AS auditorium_floor,
 	CONCAT_WS(' ', t.last_name, t.first_name, t.patronymic) AS teacher_name
 FROM public.lessons_schedule AS ls
--- LEFT JOIN public.study_groups AS sg ON ls.study_group_id = sg.id
 LEFT JOIN public.teachers AS t ON ls.teacher_id = t.id
 LEFT JOIN public.auditoriums AS a ON ls.auditorium_id = a.id
 WHERE
